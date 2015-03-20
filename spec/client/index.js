6afbc391b10ed382c33a144dc639deb171bb1b6c
Peter Schroedl <peter_schroedl@me.com>
2015-03-20 12:15:20 -0700

    Fix client tests using jsdom

--- spec/client/index.js
+++ spec/client/index.js
@@ -1,9 +1,11 @@
 var assert = require('chai').assert;
 var expect = require('chai').expect;
-var slateApp = require('../../src/client/index.js');
+var jsdom = require('mocha-jsdom')
 
 describe('client index.js', function(){
+	jsdom();
 	it('should have a function named onReady', function(){
+		var slateApp = require('../../src/client/index.js');
 		expect(typeof slateApp.onReady).to.equal('function');
 	});
 });
