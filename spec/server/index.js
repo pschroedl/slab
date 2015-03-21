cd777d311d0249b4da4a42840b2d955c02249bf6
Peter Schroedl <peter_schroedl@me.com>
2015-03-21 02:42:11 -0700

    Add gulp tasks for git hook. lint, jscs, and test on commit

--- spec/server/index.js
+++ spec/server/index.js
@@ -1,9 +1,8 @@
-var assert = require('chai').assert;
 var expect = require('chai').expect;
 var slateServer = require('../../src/server/index.js');
 
-describe('server index.js', function(){
-	it('should have a function named render', function(){
+describe('server index.js', function() {
+	it('should have a function named render', function() {
 		expect(typeof slateServer.render).to.equal('function');
 	});
 });
--- tasks/lint.js
+++ tasks/lint.js
@@ -1,9 +1,35 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var map = require('map-stream');
+var srcFiles = [
+	'src/client/*.js',
+	'src/server/*.js',
+	'spec/client/*.js',
+	'spec/server/*.js',
+	'tasks/*.js'
+];
 
-gulp.task('lint', function() {
-    gulp.src(['src/client/*.js', 'src/server/*.js'])
-        .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter('jshint-stylish'))
-        .pipe(jshint.reporter('fail'));
+gulp.task('lint', function(){
+	gulp.src(srcFiles)
+		.pipe(jshint('.jshintrc'))
+		.pipe(jshint.reporter('jshint-stylish'))
+		// .pipe(errorReporter());
+		.pipe(jshint.reporter('fail'));
 });
+
+gulp.task('lint_error_out', function(){
+	gulp.src(srcFiles)
+		.pipe(jshint('.jshintrc'))
+		.pipe(jshint.reporter('jshint-stylish'))
+		.pipe(errorReporter());
+		// .pipe(jshint.reporter('fail'));
+});
+
+var errorReporter = function(){
+	return map(function(file, cb){
+		if (!file.jshint.success) {
+			process.exit(1);
+		}
+		cb(null, file);
+	});
+};
