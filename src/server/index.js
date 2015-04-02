fdeff574e39959749fcf329f5d4bbb47d10dcd4a
Peter Schroedl <peter_schroedl@me.com>
2015-04-02 00:45:46 -0700

    Add minimal server logging, change port to 8080

--- src/server/index.js
+++ src/server/index.js
@@ -2,20 +2,30 @@
 
 var Hapi = require('hapi');
 var server = new Hapi.Server();
-server.connection({host: 'localhost', port : 3000});
+server.connection({host: 'localhost', port : 8080});
+
+var preHandler = function(request,next){
+    console.log('Recieved request for : ' + JSON.stringify(request.url.path));
+    return next();
+}
 
 server.route({
     method: 'GET',
     path: '/{param*}',
-    handler: {
-        directory: {
-            path: 'public',
-            index : true
+    config: {
+        pre: [{ method : preHandler }],
+        handler: {
+            directory: {
+                path: 'public',
+                index : true
+            }
         }
     }
 });
 
-server.start();
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
 
 // export for testing
 module.exports = server;
